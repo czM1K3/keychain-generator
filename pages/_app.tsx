3ed7ed3a9923c977714e30437d33257a61e3b250
Craig Ceremuga <craig@proxima.local>
2024-04-05 17:16:59 -0400

    * Add MUI dependency * Redesign Menu Form * Update form to use hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import { AppProps } from "next/app";
 import "../styles/main.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
